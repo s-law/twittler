16deccc7d5d920aa4b31a1fdba9babae83fe80a1
Steven Law <slaw@nuclearanvil.com>
2015-10-17 21:42:19 -0700

    Implemented loadWatched to present list of watched users.

--- application.js
+++ application.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   var $didits = $('#didits');
+  var $watched = $('#watched')
   var $toggleRefresh = $('#toggleRefresh');
+  var users = [];
 
   var autorefresh = false;
   var diditAutoRefresh;
@@ -12,7 +14,7 @@ $(document).ready(function() {
       var tweet = streams.home[index];
       var tweTime = tweet.created_at;
       var tweetElement = '<div class="didit" title=\"' + tweTime + '\"></div>'
-      var tweetFormatted = '@' + tweet.user + '<br />' + tweet.message + '<br /><span class="diditByline">' + tweet.user + ' didIt ' + $.timeago(tweTime) + '</span>';
+      var tweetFormatted = '@' + tweet.user + ':<br />' + tweet.message + '<br /><span class="diditByline">' + tweet.user + ' didIt ' + $.timeago(tweTime) + '</span>';
 
       var $tweet = $(tweetElement);
       $tweet.html(tweetFormatted);
@@ -21,7 +23,26 @@ $(document).ready(function() {
     }
   }
 
+  function loadWatched () {
+    for(var name in streams.users) {
+      users.push(name);
+    }
+    users.sort(function (a, b) {
+      return a - b;
+    })
+
+    $watched.html('');
+    users.forEach(function(user) {
+      var userElement = '<li id=\"' + user + '\"></li>'
+
+      var $user = $(userElement);
+      $user.html(user);
+      $user.appendTo($watched);
+    });
+  }
+
   loadDidits();
+  loadWatched();
 
   $toggleRefresh.on('click', function() {
     $('#refreshDidits').slideToggle();
