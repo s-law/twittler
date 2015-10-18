4cb1324fab1c3b8520752da700167c3ce28dc43e
Steven Law <slaw@nuclearanvil.com>
2015-10-17 18:13:09 -0700

    Moved javascript block from index.html to application.js. Moved Roboto stylesheet link from index.html to styles.css.

---
+++ application.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  var $didits = $('#didits');
+  var $toggleRefresh = $('#toggleRefresh');
+
+  var autorefresh = false;
+  var diditAutoRefresh;
+
+  function loadDidits() {
+    $didits.html('');
+    var index = streams.home.length - 1;
+    while(index >= 0) {
+      var tweet = streams.home[index];
+      var tweTime = tweet.created_at;
+      var tweetElement = '<div class="didit" title=\"' + tweTime + '\"></div>'
+      var tweetFormatted = tweet.message + '<br />' + '@' + tweet.user + ' didIt ' + $.timeago(tweTime);
+
+      var $tweet = $(tweetElement);
+      $tweet.html(tweetFormatted);
+      $tweet.appendTo($didits);
+      index -= 1;
+    }
+  }
+
+  loadDidits();
+
+  $toggleRefresh.on('click', function() {
+    $('#refreshDidits').slideToggle();
+    if (!autorefresh) {
+      autorefresh = true;
+      $toggleRefresh.text('Auto-refresh: ON');
+      diditAutoRefresh = setInterval(loadDidits, 30000);
+    }
+    else {
+      autorefresh = false;
+      $toggleRefresh.text('Auto-refresh: OFF');
+      clearInterval(diditAutoRefresh);
+    }
+  })
+
+  $('#refreshDidits').on('click', function() {
+    loadDidits();
+  })
+
+});
